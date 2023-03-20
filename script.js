cbccac19f7c43b75911e12d3393c25634e0e8ec8
Jeevan Johal <jkjohal@ucdavis.edu>
2023-03-20 00:10:34 -0700

    clean up code

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var weatherApiKey = 'e7c18c7a2692c89d279349d1653aec75';
 
 
 //get elements from HTML doc
-var searchForm = document.querySelector('#search-form');
-var searchInput = document.querySelector('#search-input');
-var todayContainer = document.querySelector('#today');
-var forecastContainer = document.querySelector('#forecast');
-var searchHistoryContainer = document.querySelector('#history');
+var searchForm = document.getElementById('search-form');
+var searchInput = document.getElementById('search-input');
+var todayContainer = document.getElementById('today');
+var forecastContainer = document.getElementById('forecast');
+var searchHistoryContainer = document.getElementById('history');
 
 //day.js
 dayjs.extend(window.dayjs_plugin_utc);
